7d8d51be63e7ddbd541bf944188ad2836ca6c5cb
edanielbluford <e.daniel.bluford@outlook.com>
2019-11-11 10:34:37 -0500

    laptop updates

--- basics/variables.js
+++ basics/variables.js
@@ -1,1 +1,11 @@
 let petName = "Watson";
+
+console.log(petName);
+
+const petlist = ["Watson", "Bruce", "Abby"];
+
+for (let index = 0; index < petlist.length; index++) {
+  const element = petlist[index];
+
+  console.log(element);
+}
